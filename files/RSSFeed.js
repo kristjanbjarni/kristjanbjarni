ccbfba281e5b422cca3ee08d62f81648ef726fab
Kristján Bjarni <kristjanbjarni@gmail.com>
2023-02-06 17:00:08 +0000

    Update RSSFeed.js

--- files/RSSFeed.js
+++ files/RSSFeed.js
@@ -1,17 +1,15 @@
 function RSSFeed(RSS_URL,count,content_id)
 {
-  if (!count) count = 3;
+  if (!count) count = 5;
   if (!content_id) content_id = 'content';
   fetch(RSS_URL)
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
   .then(data => {
-    //console.log(data);
     const items = data.querySelectorAll("item");
     let html = ``;
     for (let i=0; i<Math.min(items.length,count); i++) {
       let el = items[i];
-    //items.forEach(el => {
       html += `
           <li>
             <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
@@ -21,8 +19,7 @@ function RSSFeed(RSS_URL,count,content_id)
           </li>
       `;
     }
-    //);
-    var content = document.getElementById(content_id);
+    let content = document.getElementById(content_id);
     content.innerHTML = html;
   });
 }
